a2e2e3529cca84fe65a0cd4313932ed2afa2123f
Benjamin Johnson <bjohnson@fcusd.org>
2024-09-18 21:55:38 -0700

    Update TestString1.java
    Need to check tests still

--- src/test/java/TestString1.java
+++ src/test/java/TestString1.java
@@ -10,128 +10,157 @@ import org.junit.jupiter.api.Test;
  * The test class String1Test. Dont edit or it will mess up your grade :)
  *
  * @author Mr. Johnson
- * @version 8/12/24 v1 (a version number or a date)
+ * @version 9/18/24 v1 (a version number or a date)
  */
-public class TestString1{
-    private String1 testStrings = new String1();
+
+public class TestString2 {
+
+    String2 string2 = new String2();
 
     @Test
-    public void helloNameTest(){
-        assertEquals("Hello Bob!", testStrings.helloName("Bob"));
-        assertEquals("Hello Alice!", testStrings.helloName("Alice"));
-        assertEquals("Hello X!", testStrings.helloName("X"));
+    public void doubleCharTest() {
+        assertEquals("TThhee", string2.doubleChar("The"));
+        assertEquals("aabbcc", string2.doubleChar("abc"));
+        assertEquals("", string2.doubleChar(""));
     }
 
     @Test
-    public void makeAbbaTest(){
-        assertEquals("HiByeByeHi", testStrings.makeAbba("Hi", "Bye"));
-        assertEquals("YoAliceAliceYo", testStrings.makeAbba("Yo", "Alice"));
-        assertEquals("WhatUpUpWhat", testStrings.makeAbba("What", "Up"));
-
+    public void countCodeTest() {
+        assertEquals(1, string2.countCode("aaacodebbb"));
+        assertEquals(2, string2.countCode("cozexxcodecode"));
+        assertEquals(0, string2.countCode("abc"));
     }
 
     @Test
-    public void makeTagsTest(){
-        assertEquals("<i>Yay</i>", testStrings.makeTags("i", "Yay"));
-        assertEquals("<i>Hello</i>", testStrings.makeTags("i", "Hello"));
-        assertEquals("<cite>Yay</cite>", testStrings.makeTags("cite", "Yay"));
+    public void bobThereTest() {
+        assertEquals(true, string2.bobThere("abcbob"));
+        assertEquals(false, string2.bobThere("bobby"));
+        assertEquals(false, string2.bobThere("xyz"));
     }
 
     @Test
-    public void makeOutWordTest(){
-        assertEquals("<<Yay>>", testStrings.makeOutWord("<<>>", "Yay"));
-        assertEquals("<<WooHoo>>", testStrings.makeOutWord("<<>>", "WooHoo"));
-        assertEquals("[[word]]", testStrings.makeOutWord("[[]]", "word"));
+    public void repeatEndTest() {
+        assertEquals("lolo", string2.repeatEnd("Hello", 2));
+        assertEquals("abab", string2.repeatEnd("ab", 2));
+        assertEquals("xxx", string2.repeatEnd("xxxx", 3));
     }
 
     @Test
-    public void extraEndTest(){
-        assertEquals("lololo", testStrings.extraEnd("Hello"));
-        assertEquals("ababab", testStrings.extraEnd("ab"));
-        assertEquals("HiHiHi", testStrings.extraEnd("Hi"));
+    public void prefixAgainTest() {
+        assertEquals(true, string2.prefixAgain("ababc"));
+        assertEquals(false, string2.prefixAgain("ababc"));
+        assertEquals(true, string2.prefixAgain("ababcabc"));
     }
 
     @Test
-    public void firstTwoTest(){
-        assertEquals("He", testStrings.firstTwo("Hello"));
-        assertEquals("ab", testStrings.firstTwo("abcdefg"));
-        assertEquals("ab", testStrings.firstTwo("ab"));
+    public void sameStarCharTest() {
+        assertEquals(true, string2.sameStarChar("xy*abc*def"));
+        assertEquals(false, string2.sameStarChar("xy*ab*cd*ef"));
+        assertEquals(true, string2.sameStarChar("*abc*def*"));
     }
 
     @Test
-    public void firstHalfTest(){
-        assertEquals("Woo", testStrings.firstHalf("WooHoo"));
-        assertEquals("Hello", testStrings.firstHalf("HelloThere"));
-        assertEquals("abc", testStrings.firstHalf("abcdef"));
+    public void starOutTest() {
+        assertEquals("ad", string2.starOut("ab*cd"));
+        assertEquals("ab", string2.starOut("ab*cd*ef"));
+        assertEquals("", string2.starOut("**"));
     }
 
     @Test
-    public void withoutEndTest(){
-        assertEquals("ell", testStrings.withoutEnd("Hello"));
-        assertEquals("av", testStrings.withoutEnd("java"));
-        assertEquals("odin", testStrings.withoutEnd("coding"));
+    public void countHiTest() {
+        assertEquals(2, string2.countHi("abc hi ho hi"));
+        assertEquals(1, string2.countHi("hi there"));
+        assertEquals(0, string2.countHi("hello"));
     }
 
     @Test
-    public void comboStringTest(){
-        assertEquals("hiHellohi", testStrings.comboString("hi", "Hello"));
-        assertEquals("hiHellohi", testStrings.comboString("Hello", "hi"));
-        assertEquals("baaab", testStrings.comboString("aaa", "b"));
+    public void endOtherTest() {
+        assertEquals(true, string2.endOther("Hiabc", "abc"));
+        assertEquals(true, string2.endOther("abc", "Hiabc"));
+        assertEquals(false, string2.endOther("abc", "xyz"));
     }
 
     @Test
-    public void middleThreeTest(){
-        assertEquals("and", testStrings.middleThree("Candy"));
-        assertEquals("and", testStrings.middleThree("and"));
-        assertEquals("lvi", testStrings.middleThree("solving"));
+    public void xyBalanceTest() {
+        assertEquals(true, string2.xyBalance("aaxbby"));
+        assertEquals(false, string2.xyBalance("aaxbbyx"));
+        assertEquals(true, string2.xyBalance("xaxbby"));
     }
 
     @Test
-    public void extraFrontTest(){
-        assertEquals("HeHeHe", testStrings.extraFront("Hello"));
-        assertEquals("ababab", testStrings.extraFront("ab"));
-        assertEquals("HHH", testStrings.extraFront("H"));
+    public void repeatFrontTest() {
+        assertEquals("ChocChocChocChoc", string2.repeatFront("Chocolate", 4));
+        assertEquals("ab", string2.repeatFront("abcd", 2));
+        assertEquals("a", string2.repeatFront("a", 1));
     }
 
     @Test
-    public void left2Test(){
-        assertEquals("lloHe", testStrings.left2("Hello"));
-        assertEquals("vaja", testStrings.left2("java"));
-        assertEquals("Hi", testStrings.left2("Hi"));
+    public void xyzMiddleTest() {
+        assertEquals(true, string2.xyzMiddle("abcxyz123"));
+        assertEquals(false, string2.xyzMiddle("abcxyz12"));
+        assertEquals(true, string2.xyzMiddle("xyz123xyz"));
     }
 
     @Test
-    public void hasBadTest(){
-        assertEquals(true, testStrings.hasBad("badxx"));
-        assertEquals(true, testStrings.hasBad("xbadxx"));
-        assertEquals(false , testStrings.hasBad("xxbadxx"));
+    public void oneTwoTest() {
+        assertEquals("bcddef", string2.oneTwo("abcdef"));
+        assertEquals("xyzxyz", string2.oneTwo("xyzxyz"));
+        assertEquals("", string2.oneTwo("ab"));
     }
 
     @Test
-    public void conCatTest(){
-        assertEquals("abcat", testStrings.conCat("abc", "cat"));
-        assertEquals("dogcat", testStrings.conCat("dog", "cat"));
-        assertEquals("abc", testStrings.conCat("abc", ""));
+    public void plusOutTest() {
+        assertEquals("---+++---", string2.plusOut("abcxyz", "xyz"));
+        assertEquals("a--a", string2.plusOut("aabb", "ab"));
+        assertEquals("---", string2.plusOut("abcdef", "xyz"));
     }
 
     @Test
-    public void minCatTest(){
-        assertEquals("loHi", testStrings.minCat("Hello", "Hi"));
-        assertEquals("ellojava", testStrings.minCat("Hello", "java"));
-        assertEquals("javaello", testStrings.minCat("java", "Hello"));
+    public void catDogTest() {
+        assertEquals(true, string2.catDog("catdogcat"));
+        assertEquals(false, string2.catDog("catdog"));
+        assertEquals(true, string2.catDog("dogcat"));
     }
 
     @Test
-    public void withoutXTest(){
-        assertEquals("Hi", testStrings.withoutX("xHix"));
-        assertEquals("Hi", testStrings.withoutX("xHi"));
-        assertEquals("Hxi", testStrings.withoutX("Hxix"));
+    public void xyzThereTest() {
+        assertEquals(true, string2.xyzThere("abcxyz"));
+        assertEquals(false, string2.xyzThere("abcxxyz"));
+        assertEquals(true, string2.xyzThere("xyz"));
     }
 
     @Test
-    public void deFrontTest(){
-        assertEquals("llo", testStrings.deFront("Hello"));
-        assertEquals("va", testStrings.deFront("java"));
-        assertEquals("aay", testStrings.deFront("away"));
+    public void mixStringTest() {
+        assertEquals("a1b2c3", string2.mixString("abc", "123"));
+        assertEquals("a1b2c", string2.mixString("abc", "12"));
+        assertEquals("abcd", string2.mixString("abcd", ""));
+    }
+
+    @Test
+    public void repeatSeparatorTest() {
+        assertEquals("WordXWordXWord", string2.repeatSeparator("Word", "X", 3));
+        assertEquals("abcbcbcb", string2.repeatSeparator("abc", "b", 3));
+        assertEquals("xyz", string2.repeatSeparator("xyz", "X", 1));
+    }
+
+    @Test
+    public void getSandwichTest() {
+        assertEquals("jam", string2.getSandwich("breadjambread"));
+        assertEquals("", string2.getSandwich("breadbread"));
+        assertEquals("xyz", string2.getSandwich("breadxyzbread"));
+    }
+
+    @Test
+    public void zipZapTest() {
+        assertEquals("zz", string2.zipZap("zipzap"));
+        assertEquals("z", string2.zipZap("zippzap"));
+        assertEquals("zap", string2.zipZap("zaps"));
+    }
+
+    @Test
+    public void wordEndsTest() {
+        assertEquals("aabcxxbc", string2.wordEnds("abcxxabc", "abc"));
+        assertEquals("aabc", string2.wordEnds("abcxxabc", "abc"));
+        assertEquals("", string2.wordEnds("abcdef", "ghi"));
     }
 }
