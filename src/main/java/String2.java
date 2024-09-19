01f931dd08089a4ff7797186e99f7cf18971b6cc
Benjamin Johnson <bjohnson@fcusd.org>
2024-09-18 21:51:50 -0700

    Update and rename String1.java to String2.java

---
+++ src/main/java/String2.java
@@ -0,0 +1,155 @@
+package src.main.java;
+
+
+public class String2 {
+
+    /**
+     * Given a string, return a string where for every character in the original, there are two characters.
+     */
+    public String doubleChar(String str) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Return the number of times that the string "code" appears anywhere in the given string,
+     * where the letters can be separated by any number of characters.
+     */
+    public int countCode(String str) {
+        return 0; // Placeholder return
+    }
+
+    /**
+     * Return true if the string contains the string "bob" anywhere,
+     * but only if it is not part of the substring "bobby".
+     */
+    public boolean bobThere(String str) {
+        return false; // Placeholder return
+    }
+
+    /**
+     * Given a string and a non-negative integer n, return a string made of n copies of the last n characters of the original string.
+     */
+    public String repeatEnd(String str, int n) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Given a string, return true if the prefix (first 3 characters) appears somewhere else in the string, excluding the first occurrence.
+     */
+    public boolean prefixAgain(String str) {
+        return false; // Placeholder return
+    }
+
+    /**
+     * Return true if every '*' in the string is followed by the same character and before another '*'.
+     */
+    public boolean sameStarChar(String str) {
+        return false; // Placeholder return
+    }
+
+    /**
+     * Given a string, return a version where all the '*' characters and their surrounding characters are removed.
+     */
+    public String starOut(String str) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Return the number of times that the string "hi" appears in the given string.
+     */
+    public int countHi(String str) {
+        return 0; // Placeholder return
+    }
+
+    /**
+     * Given two strings, return true if either string appears at the end of the other string, ignoring case.
+     */
+    public boolean endOther(String a, String b) {
+        return false; // Placeholder return
+    }
+
+    /**
+     * Given a string, return true if all the 'x' characters in the string are followed by at least one 'y'.
+     */
+    public boolean xyBalance(String str) {
+        return false; // Placeholder return
+    }
+
+    /**
+     * Given a string and a non-negative integer n, return a string made of the first n characters of the string repeated n times.
+     */
+    public String repeatFront(String str, int n) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Given a string, return true if the substring "xyz" appears in the middle of the string.
+     */
+    public boolean xyzMiddle(String str) {
+        return false; // Placeholder return
+    }
+
+    /**
+     * Given a string, return a new string where every group of 3 characters is replaced by the substring starting from index 1 and ending at index 3.
+     */
+    public String oneTwo(String str) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Given a string and a non-empty string word, return a string where every occurrence of word is replaced by '+',
+     * and all other characters are replaced by '-'.
+     */
+    public String plusOut(String str, String word) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Given a string, return true if the number of occurrences of "cat" is equal to the number of occurrences of "dog".
+     */
+    public boolean catDog(String str) {
+        return false; // Placeholder return
+    }
+
+    /**
+     * Return true if the string contains an "xyz" substring, but not immediately after another "x".
+     */
+    public boolean xyzThere(String str) {
+        return false; // Placeholder return
+    }
+
+    /**
+     * Given two strings, return a new string consisting of the characters from both strings, alternating one from each.
+     */
+    public String mixString(String a, String b) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Given a string and a non-negative integer n, return a string made of n copies of the string separated by the given separator string.
+     */
+    public String repeatSeparator(String word, String sep, int count) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Given a string, return the substring that is between the first and last occurrence of "bread".
+     */
+    public String getSandwich(String str) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Given a string, return a string where every instance of "zip" is replaced with "z" and every instance of "zap" is replaced with "z".
+     */
+    public String zipZap(String str) {
+        return ""; // Placeholder return
+    }
+
+    /**
+     * Given a string and a non-empty substring word, return a string where every occurrence of word is surrounded by the characters immediately before and after it in the original string.
+     */
+    public String wordEnds(String str, String word) {
+        return ""; // Placeholder return
+    }
+}
